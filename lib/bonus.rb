d9794209a74563e784d715b5777f838a6f0d6d00
zachary mines <zmines@wesleyan.edu>
2020-06-27 20:51:53 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,9 @@ def bonus
    }
   }
 
-  epic_tragedy[:montague][:hero][:status]
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  
 
   
 
