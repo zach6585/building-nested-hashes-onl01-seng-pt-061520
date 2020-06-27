276127413f0fa57ae70bbf22899a99e3e1f2bcca
zachary mines <zmines@wesleyan.edu>
2020-06-27 20:45:40 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Benvolio", age: "17", attitude: "worried"}
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
