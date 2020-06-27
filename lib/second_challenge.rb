514ccc63fa06a7688d2ba7f21d9660b691e29d95
zachary mines <zmines@wesleyan.edu>
2020-06-27 20:34:47 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,6 +2,9 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       partiarch: {}
+      matriarch: {}
+      hero: {}
+      hero_friends: {}
       }, 
     :capulet => {
       #your key/value pairs here!
