7e44c86002c7546713a6b06f2abeccaba2e861bf
zachary mines <zmines@wesleyan.edu>
2020-06-27 20:45:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,7 +14,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {name: "Benvolio", age: "17", attitude: "worried"}
       ]
    }
 }
